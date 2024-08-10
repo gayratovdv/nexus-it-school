8370648c53a5988f4b2152548e1f2a01b56bc5b1
Muhammad ali <mahmudjonovm16@gmail.com>
2024-08-10 14:01:08 +0500

    Contact bitdi

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Home from './pages/Home/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NotFound from "./pages/NotFound/Error";
 import Contact from "./pages/contact/contact";
+import About from './pages/about/about';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
         <Route path='/*' element={<NotFound />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
